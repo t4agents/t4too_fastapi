1182ecc2034f02837f104d994a1faa97338e1736
aiaccfin <aiaccfin@gmail.com>
2026-04-23 12:35:48 -0400

    v

--- app/api/ai/__init__.py
+++ app/api/ai/__init__.py
@@ -3,8 +3,7 @@ from fastapi import APIRouter
 from .ai_brain import routerRou
 from .ai_guardrail import guardrailRou
 from .ai_langgraph import langRou
-from .ai_rag import aiRagRou
-from .ai_rag_eval import ragEvalRou
+from .rag3_eval import ragEvalRou
 
 from .rag1_unit import ragUnitRou
 from .rag2_llm import ragLLMRou
@@ -13,8 +12,8 @@ rouAI = APIRouter()
 
 rouAI.include_router(ragUnitRou, prefix="/rag1unit", tags=["rag-basic"])
 rouAI.include_router(ragLLMRou, prefix="/rag2llm", tags=["rag-llm"])
+rouAI.include_router(ragEvalRou, prefix="/rag3eval", tags=["rag-eval"])
 
 rouAI.include_router(guardrailRou, prefix="/guardrail", tags=["ai-guardrail"])
-rouAI.include_router(ragEvalRou, prefix="/rag_eval", tags=["ai-rag-eval"])
 rouAI.include_router(routerRou, prefix="/brain", tags=["ai-brain"])
 rouAI.include_router(langRou, prefix="/langgraph", tags=["ai-langgraph"])
